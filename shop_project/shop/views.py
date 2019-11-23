c001b14282abac0902f2bd112877af2f9020e04b
piotr-ole <piotrek.olesiejuk@gmail.com>
2019-11-23 15:11:41 +0100

    Added client login

--- shop_project/shop/views.py
+++ shop_project/shop/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 shop_introduction = "Welcome in the shop: \"Zoo Animations For Curious Animators\"!"
 
@@ -8,11 +9,12 @@ def index(request):
                        "2. Change your URL to /shop/your_name to enter your shop account."]
     return HttpResponse('<br>'.join(response_lines))
 
+
 def shop(request):
     global shop_introduction
-    return HttpResponse(shop_introduction)
+    return render(request, r'shop\main.html')
 
 def shop_account(request, name):
     global shop_introduction
     response_lines = [shop_introduction, "Hi, " + name + "!"]
-    return HttpResponse('<br>'.join(response_lines))
+    return render(request, r'shop\client.html', {'client_name' : name})
