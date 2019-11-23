20b2ce62e6b690cb207c65eb280e011de943a27b
piotr-ole <piotrek.olesiejuk@gmail.com>
2019-11-23 22:15:41 +0100

    Added products and product details

--- shop_project/shop/urls.py
+++ shop_project/shop/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('shop/', views.shop),
-    path('shop/<str:name>', views.shop_account)
+    path('shop/<str:name>', views.shop_account),
+    path('products/', views.products_list),
+    path('products/<int:product_id>', views.product_details)
 ]
--- shop_project/shop/views.py
+++ shop_project/shop/views.py
@@ -3,10 +3,20 @@ from django.shortcuts import render
 
 shop_introduction = "Welcome in the shop: \"Zoo Animations For Curious Animators\"!"
 
+products = [
+ {"name": "Książka o samochodach",
+"price": 100, "id": 1, "description": "Wstęp do obsługi Malucha", "author" : "Bernard Żuk"},
+ {"name": "Film o helikopterach",
+"price": 50, "id": 2, "description": "Pościgi i wybuchy, w roli głównej Tomasz Karolak", "author" : "Patryk Vegeta"},
+ {"name": "Komiks o statkach",
+"price": 15, "id": 3, "description": "Finezyjna przygoda w świecie żeglugi", "author" : "Katarzyna Gafel"},
+]
+
 def index(request):
     response_lines = ["Welcome to my Shop Project!",
-                       "1. Change your URL to /shop to enter the shop.",
-                       "2. Change your URL to /shop/your_name to enter your shop account."]
+                       "1. Change your URL to /shop to enter the shop and log in.",
+                       "2. Change your URL to /products to browse our shop",
+                       ]
     return HttpResponse('<br>'.join(response_lines))
 
 
@@ -16,5 +26,12 @@ def shop(request):
 
 def shop_account(request, name):
     global shop_introduction
-    response_lines = [shop_introduction, "Hi, " + name + "!"]
     return render(request, r'shop\client.html', {'client_name' : name})
+
+def products_list(request):
+    global products
+    return render(request, r"shop\list.html", {"products": products})
+
+def product_details(request, product_id):
+    global products
+    return render(request, r"shop\product_details.html", {"products": products , "product_id" : product_id})
