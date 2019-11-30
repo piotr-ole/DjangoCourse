414602fff353cb94bca148723dd93c0a10de1ea7
piotr-ole <piotrek.olesiejuk@gmail.com>
2019-11-30 16:13:24 +0100

    PD3 done

--- shop_project/shop/models.py
+++ shop_project/shop/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    
+    name = models.CharField(max_length=100)
+    price = models.FloatField(default=0)
+    description = models.CharField(default='brak opisu', max_length=500)
+    weight = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+
+    DELIVERY_CHOICES = [
+        ('letter', 'letter'),
+        ('package', 'package'),
+        ('courier', 'courier'),
+    ]
+
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=70)
+    street = models.CharField(max_length=70)
+    building_number = models.IntegerField()
+    apartament_number = models.IntegerField(default=None, blank=True, null=True)
+    delivery = models.CharField(max_length=7, choices=DELIVERY_CHOICES)
+
+    def __str__(self):
+        return str(self.id) + '_' + self.name + '_' + self.surname
--- shop_project/shop/views.py
+++ shop_project/shop/views.py
@@ -1,37 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 shop_introduction = "Welcome in the shop: \"Zoo Animations For Curious Animators\"!"
 
-products = [
- {"name": "Książka o samochodach",
-"price": 100, "id": 1, "description": "Wstęp do obsługi Malucha", "author" : "Bernard Żuk"},
- {"name": "Film o helikopterach",
-"price": 50, "id": 2, "description": "Pościgi i wybuchy, w roli głównej Tomasz Karolak", "author" : "Patryk Vegeta"},
- {"name": "Komiks o statkach",
-"price": 15, "id": 3, "description": "Finezyjna przygoda w świecie żeglugi", "author" : "Katarzyna Gafel"},
-]
-
 def index(request):
-    response_lines = ["Welcome to my Shop Project!",
-                       "1. Change your URL to /shop to enter the shop and log in.",
-                       "2. Change your URL to /products to browse our shop",
-                       ]
-    return HttpResponse('<br>'.join(response_lines))
+    return render(request, 'shop/index.html')
 
 
 def shop(request):
     global shop_introduction
-    return render(request, r'shop\main.html')
+    return render(request, 'shop/main.html')
 
 def shop_account(request, name):
     global shop_introduction
-    return render(request, r'shop\client.html', {'client_name' : name})
+    return render(request, 'shop/client.html', {'client_name' : name})
 
 def products_list(request):
-    global products
-    return render(request, r"shop\list.html", {"products": products})
+    products = Product.objects.order_by('id')
+    context = {'products': products}
+    return render(request, "shop/list.html", context)
+
 
 def product_details(request, product_id):
-    global products
-    return render(request, r"shop\product_details.html", {"products": products , "product_id" : product_id})
+    product = Product.objects.get(id=product_id)
+    context = {'product': product}
+    return render(request, "shop/product_details.html", context)
+
+
